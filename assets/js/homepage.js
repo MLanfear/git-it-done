b1ab895b9949cdaa494a19de24deb0a27dd7486c
MLanfear <maxjlanfear@gmail.com>
2022-08-16 20:53:37 -0700

    done for the night

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,21 +1,40 @@
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-    
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
-    });
-};
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-getUserRepos();
+
+    var getUserRepos = function(user) {
+        // format the github api url
+        var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+        // make a request to the url
+        fetch(apiUrl).then(function(response) {
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        });
+    };
+
     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
-    
-    console.log("outside");
+
+    var formSubmitHandler = function(event) {
+        event.preventDefault();
+
+        // get value from input elements
+        var username = nameInputEl.value.trim();
+
+        if (username) {
+            getUserRepos(username);
+            nameInputEl.value = "";
+        } else {
+            alert("Please enter a Github username");
+        }
+        console.log(event);
+    };
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
 
